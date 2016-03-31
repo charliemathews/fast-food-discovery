557ae420d8611d54d8e3c178604aab65f2f841f8
Charlie Mathews <charlie@charliemathews.com>
2016-03-31 00:03:59 -0400

    Start to places model.

--- fast-food-discovery/Places.swift
+++ fast-food-discovery/Places.swift
@@ -1,9 +1,34 @@
-//
-//  Places.swift
-//  fast-food-discovery
-//
-//  Created by Charles Mathews on 3/30/16.
-//  Copyright © 2016 Charlie Mathews. All rights reserved.
-//
+/*
+ Copywrite Grove City College 2016
+ Authored by Charlie Mathews & Sarah Burgess
+ */
+
+// example query for reference
+// https://maps.googleapis.com/maps/api/place/textsearch/json?query=unique+fast+food+near+16127&key=AIzaSyDsTvS1RyzH7wVbYhqXGM276SWlnRU5-HA
 
 import Foundation
+
+class Places {
+    
+    let base = "https://maps.googleapis.com/maps/api/place/"
+    let format = "json"
+    let key = "AIzaSyDsTvS1RyzH7wVbYhqXGM276SWlnRU5-HA"
+    
+    var results : [Place] = []
+    
+    init() {
+        
+    }
+    
+    func getPlacesTextQuery(query : String, location : String) {
+    
+        //build query
+        var queryURL : String = base
+        queryURL += "textsearch/"
+        queryURL += format
+        queryURL += "?query=" + query + "+near+" + location
+        queryURL += "&key=" + key
+        
+        
+    }
+}
